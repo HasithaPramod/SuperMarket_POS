0d8a8225ae4400e31dd23f7354a7b1566e242517
Amaya Kumarasiri <amayakumarasiri7@gmail.com>
2023-01-21 16:03:02 +0530

    Stock Details

---
+++ Stock Details.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace hasitha
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dtGrdvCustomerDetails = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnStoreCreditRewards = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrdvCustomerDetails)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtGrdvCustomerDetails
+            // 
+            this.dtGrdvCustomerDetails.AllowUserToAddRows = false;
+            this.dtGrdvCustomerDetails.AllowUserToDeleteRows = false;
+            this.dtGrdvCustomerDetails.AllowUserToResizeColumns = false;
+            this.dtGrdvCustomerDetails.AllowUserToResizeRows = false;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dtGrdvCustomerDetails.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dtGrdvCustomerDetails.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtGrdvCustomerDetails.BackgroundColor = System.Drawing.Color.White;
+            this.dtGrdvCustomerDetails.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtGrdvCustomerDetails.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dtGrdvCustomerDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Times New Roman", 12F);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtGrdvCustomerDetails.DefaultCellStyle = dataGridViewCellStyle7;
+            this.dtGrdvCustomerDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtGrdvCustomerDetails.Location = new System.Drawing.Point(0, 0);
+            this.dtGrdvCustomerDetails.Margin = new System.Windows.Forms.Padding(2);
+            this.dtGrdvCustomerDetails.Name = "dtGrdvCustomerDetails";
+            this.dtGrdvCustomerDetails.ReadOnly = true;
+            this.dtGrdvCustomerDetails.RowHeadersWidth = 51;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            this.dtGrdvCustomerDetails.RowsDefaultCellStyle = dataGridViewCellStyle8;
+            this.dtGrdvCustomerDetails.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtGrdvCustomerDetails.Size = new System.Drawing.Size(883, 399);
+            this.dtGrdvCustomerDetails.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.dtGrdvCustomerDetails, "To Update Customer Info Please click on row");
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.splitContainer1.Panel1.Controls.Add(this.btnStoreCreditRewards);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dtGrdvCustomerDetails);
+            this.splitContainer1.Size = new System.Drawing.Size(883, 450);
+            this.splitContainer1.SplitterDistance = 48;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // btnStoreCreditRewards
+            // 
+            this.btnStoreCreditRewards.BackColor = System.Drawing.Color.White;
+            this.btnStoreCreditRewards.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnStoreCreditRewards.FlatAppearance.BorderSize = 0;
+            this.btnStoreCreditRewards.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnStoreCreditRewards.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Chartreuse;
+            this.btnStoreCreditRewards.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStoreCreditRewards.Location = new System.Drawing.Point(880, 2);
+            this.btnStoreCreditRewards.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStoreCreditRewards.Name = "btnStoreCreditRewards";
+            this.btnStoreCreditRewards.Size = new System.Drawing.Size(8, 36);
+            this.btnStoreCreditRewards.TabIndex = 31;
+            this.btnStoreCreditRewards.Text = "Store Credit Rewards";
+            this.btnStoreCreditRewards.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStoreCreditRewards.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStoreCreditRewards.UseVisualStyleBackColor = false;
+            this.btnStoreCreditRewards.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Stock Details";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 800;
+            this.toolTip1.AutoPopDelay = 80000;
+            this.toolTip1.BackColor = System.Drawing.Color.OliveDrab;
+            this.toolTip1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.toolTip1.InitialDelay = 1;
+            this.toolTip1.ReshowDelay = 1;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrdvCustomerDetails)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtGrdvCustomerDetails;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnStoreCreditRewards;
+        private System.Windows.Forms.Label label1;
+    }
+}
