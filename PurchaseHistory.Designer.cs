b370d954b5f28c22c4f00a6a45b2ee321c4ea854
Naveen Tharaka <94618938+NaveenTharak@users.noreply.github.com>
2023-01-22 11:19:39 +0530

    PurchaseHistory Updated

--- PurchaseHistory.Designer.cs
+++ PurchaseHistory.Designer.cs
@@ -34,7 +34,6 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.label1 = new System.Windows.Forms.Label();
             this.dtStartDate = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
             this.dtEndDate = new System.Windows.Forms.DateTimePicker();
@@ -55,13 +54,13 @@
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.Window;
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
             this.splitContainer1.Panel1.Controls.Add(this.dtStartDate);
             this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.dtEndDate);
@@ -71,36 +70,29 @@
             // 
             this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Window;
             this.splitContainer1.Panel2.Controls.Add(this.datagrdpurchasehistory);
-            this.splitContainer1.Size = new System.Drawing.Size(996, 450);
+            this.splitContainer1.Size = new System.Drawing.Size(1328, 554);
             this.splitContainer1.SplitterDistance = 74;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.25F);
-            this.label1.Location = new System.Drawing.Point(814, 57);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 12);
-            this.label1.TabIndex = 91;
-            this.label1.Text = "Click on the Row to delete each record";
             // 
             // dtStartDate
             // 
             this.dtStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
             this.dtStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtStartDate.Location = new System.Drawing.Point(51, 26);
+            this.dtStartDate.Location = new System.Drawing.Point(68, 32);
+            this.dtStartDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtStartDate.Name = "dtStartDate";
-            this.dtStartDate.Size = new System.Drawing.Size(130, 21);
+            this.dtStartDate.Size = new System.Drawing.Size(172, 25);
             this.dtStartDate.TabIndex = 90;
             this.toolTip1.SetToolTip(this.dtStartDate, "Please Select  Date 2 \r\n \r\nExample:  2014-10-29\r\nyyyy-mm-dd");
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(187, 32);
+            this.label3.Location = new System.Drawing.Point(249, 39);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.Size = new System.Drawing.Size(24, 16);
             this.label3.TabIndex = 89;
             this.label3.Text = "To";
             // 
@@ -108,9 +100,10 @@
             // 
             this.dtEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
             this.dtEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtEndDate.Location = new System.Drawing.Point(213, 26);
+            this.dtEndDate.Location = new System.Drawing.Point(284, 32);
+            this.dtEndDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtEndDate.Name = "dtEndDate";
-            this.dtEndDate.Size = new System.Drawing.Size(141, 21);
+            this.dtEndDate.Size = new System.Drawing.Size(187, 25);
             this.dtEndDate.TabIndex = 87;
             this.toolTip1.SetToolTip(this.dtEndDate, "Please Select  Date 2 \r\n \r\nExample:  2014-10-29\r\nyyyy-mm-dd");
             // 
@@ -118,9 +111,10 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
-            this.label9.Location = new System.Drawing.Point(48, 11);
+            this.label9.Location = new System.Drawing.Point(64, 14);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(123, 13);
+            this.label9.Size = new System.Drawing.Size(141, 15);
             this.label9.TabIndex = 88;
             this.label9.Text = "Purchase History by date";
             // 
@@ -156,6 +150,7 @@
             this.datagrdpurchasehistory.DefaultCellStyle = dataGridViewCellStyle3;
             this.datagrdpurchasehistory.Dock = System.Windows.Forms.DockStyle.Fill;
             this.datagrdpurchasehistory.Location = new System.Drawing.Point(0, 0);
+            this.datagrdpurchasehistory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.datagrdpurchasehistory.Name = "datagrdpurchasehistory";
             this.datagrdpurchasehistory.ReadOnly = true;
             this.datagrdpurchasehistory.RowHeadersVisible = false;
@@ -164,7 +159,7 @@
             dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             this.datagrdpurchasehistory.RowsDefaultCellStyle = dataGridViewCellStyle4;
             this.datagrdpurchasehistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.datagrdpurchasehistory.Size = new System.Drawing.Size(996, 372);
+            this.datagrdpurchasehistory.Size = new System.Drawing.Size(1328, 475);
             this.datagrdpurchasehistory.TabIndex = 2;
             this.toolTip1.SetToolTip(this.datagrdpurchasehistory, "Click on Row to delete each record");
             // 
@@ -180,11 +175,12 @@
             // 
             // PurchaseHistory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(996, 450);
+            this.ClientSize = new System.Drawing.Size(1328, 554);
             this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PurchaseHistory";
             this.Text = "Form1";
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -200,7 +196,6 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DateTimePicker dtStartDate;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Label label3;
